159f46b83005246099d654fdcb4850845a0c3014
Romeo <rommar@jpj.com>
2022-03-19 22:25:02 +0800

    Removed Dashboard View

--- src/router/index.js
+++ src/router/index.js
@@ -7,14 +7,6 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
-  },
-  {
-    path: '/dashboard',
-    name: 'adashboardbout',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/DashboardView.vue')
   }
 ]
 
--- src/views/DashboardView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="dashboard">
-    
-  </div>
-</template>
