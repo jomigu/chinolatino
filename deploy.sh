e0772afdd5025fa7e79b071ffb065f009d4a27d5
Romeo <rommar@jpj.com>
2022-03-19 21:23:19 +0800

    Updated script on build

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-export ORDER_FORM_URL=https://script.google.com/macros/s/AKfycby98lwk4hb8SN1Mls8il0W6ksDN0xkJYxWX7b1Dw_ehFJY0F-QA-1jijGvOtvc2zSRV/exec
-export CLIENT_ID=18495362772-nbpqq763ct061mfc07vtaaojkt0h2eir.apps.googleusercontent.com
-
-npm run deploy
